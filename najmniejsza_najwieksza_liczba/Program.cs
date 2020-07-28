619c941cbece7277c4eb95b339d918701352e115
lechu0715 <lechu0715@gmail.com>
2020-07-28 19:39:39 +0200

    Chane 2 "fors"

--- najmniejsza_najwieksza_liczba/Program.cs
+++ najmniejsza_najwieksza_liczba/Program.cs
@@ -4,6 +4,22 @@ namespace najmniejsza_najwieksza_liczba
 {
     class Program
     {
+        /*
+         * Zadanie 1:
+            -Stwórz metodę która przyjmuje stringa, a są nimi liczby oddzielone spacjami. Na podstawie tego argumentu zwróc najwyższą wartosc i najmniejszą jako string.
+            
+            Przykład:
+            -Dla parametrów "1 2 3 4 5" zostanie zwrócone "5 1"
+            -Dla parametrów "1 2 3 4 -5" zostanie zwrócone "4 -5"
+            -Dla parametrów "14" zostanie zwrócone "14 14"
+            
+            Założenia:
+            -Możesz założyć, że przekazany przez użytkownika parametr jest zawsze prawidłowy, nie musisz pisać walidacji.
+            -Zawsze zostanie przekakazane przynajmniej jedna liczba.
+            -Zwrócisz strina, którym będą 2 liczby oddzielone spacją. Wyższy numer będzie pierwszy.
+        */
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj liczby a program wskaże największą i najmniejszą");
@@ -20,6 +36,8 @@ namespace najmniejsza_najwieksza_liczba
 
 
             var maxNumber = int.Parse(numberList[0]);
+            var minNumber = int.Parse(numberList[0]);
+
             for (int i = 0; i < numberList.Length; i++)
             {
                 
@@ -28,18 +46,13 @@ namespace najmniejsza_najwieksza_liczba
                     maxNumber = int.Parse(numberList[i]);
                 }
 
-            }
-
-            var minNumber = int.Parse(numberList[0]);
-            for (int i = 0; i < numberList.Length; i++)
-            {
-                
                 if (int.Parse(numberList[i]) < minNumber)
                 {
                     minNumber = int.Parse(numberList[i]);
                 }
             }
 
+            
             return $"{maxNumber} {minNumber}";
 
         }
